3c7d346e303b914de3eb456a31cc838a9e04dbec
Dundar Göc <gocdundar@gmail.com>
2020-11-22 15:57:38 +0100

    Replaced zinit load with zinit light.

--- zsh/.zinit.zsh
+++ zsh/.zinit.zsh
@@ -20,54 +20,54 @@ zstyle ':prezto:module:editor' key-bindings 'vi' "
 zinit snippet PZT::modules/editor/init.zsh
 
 zinit ice silent depth'1' id-as'auto' atclone"./install --key-bindings --completion --no-update-rc" atload"source ~/.fzf.zsh"
-zinit load junegunn/fzf
+zinit light junegunn/fzf
 
 zinit ice silent depth'1' id-as'auto' wait pick'async.zsh'
-zinit load mafredri/zsh-async
+zinit light mafredri/zsh-async
 
 zinit ice silent depth'1' id-as'auto' wait
-zinit load willghatch/zsh-saneopt
+zinit light willghatch/zsh-saneopt
 
 zinit ice silent depth'1' wait id-as"completion" blockf;
 zinit snippet PZT::modules/completion/init.zsh
 
 zinit ice silent depth'1' id-as'auto' wait atinit"export ENHANCD_DISABLE_HOME=1"
-zinit load b4b4r07/enhancd
+zinit light b4b4r07/enhancd
 
 zinit ice silent depth'1' id-as'auto' wait atload"
 _zsh_autosuggest_start
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4'
 bindkey '^ ' autosuggest-accept"
-zinit load zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-autosuggestions
 
 PS1="- "
 zinit ice silent depth'1' id-as'auto' wait"!" pick"async.zsh" src"pure.zsh"
-zinit load sindresorhus/pure
+zinit light sindresorhus/pure
 
 zinit ice silent depth"1" id-as"auto" wait"1" has"git" 
-zinit load zdharma/zsh-diff-so-fancy
+zinit light zdharma/zsh-diff-so-fancy
 
 zinit ice silent depth"1" id-as"auto" wait"1" has"git" atload"unalias gi gcf gss gclean" 
-zinit load wfxr/forgit
+zinit light wfxr/forgit
 
 zinit ice silent depth"1" id-as"auto" wait"1"
-zinit load ael-code/zsh-colored-man-pages
+zinit light ael-code/zsh-colored-man-pages
 
 zinit ice silent depth"1" id-as"auto" wait"1"
-zinit load laggardkernel/zsh-thefuck
+zinit light laggardkernel/zsh-thefuck
 
 zinit ice silent depth"1" id-as"auto" wait"1"
-zinit load mollifier/cd-gitroot
+zinit light mollifier/cd-gitroot
 
 zinit ice silent depth"1" id-as"auto" wait"1"
-zinit load hlissner/zsh-autopair
+zinit light hlissner/zsh-autopair
 
 zinit ice silent depth'1' id-as'auto' wait"1"
-zinit load tmux-plugins/tpm
+zinit light tmux-plugins/tpm
 
 # Syntax highlighter "needs" to be last.
 
 zinit ice silent depth'1' id-as'auto' wait"1" atinit"
 zpcompinit
 zpcdreplay"
-zinit load zdharma/fast-syntax-highlighting
+zinit light zdharma/fast-syntax-highlighting
