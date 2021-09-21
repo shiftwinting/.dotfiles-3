711c3b5488392a22fd7c66a4303060f7d7b5ce06
Dundar Göc <gocdundar@gmail.com>
2021-09-21 18:13:16 +0200

    changed gk to be an alias for gitk

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -61,10 +61,7 @@ alias gis="git switch"
 alias gf="git add .; git commit -m 'Quick update, squash later.'"
 alias gfn="git add .; git commit --no-verify -m 'Quick update, squash later.'"
 alias gt="git add .; git commit -m 'Test commit, will likely delete after.'"
-gk() {
-	gitk ${@:-.}
-}
-
+alias gk="gitk"
 alias gl="git log --graph --full-history --pretty --oneline"
 alias gla="gl --all"
 alias glr="forgit::log HEAD..origin"
