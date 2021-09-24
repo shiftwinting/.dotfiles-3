1de6c80207cfd707b33de1fa2155b61c92a45bac
Dundar Göc <gocdundar@gmail.com>
2021-09-24 10:06:21 +0200

    updated uncrustify location

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -391,14 +391,14 @@ build-uncrustify() {
 alias black="black -C"
 
 alias cl='clang-format -i'
-alias un='uncrustify -c $HOME/programs/neovim/contrib/uncrustify.cfg --replace --no-backup'
-alias bun='$HOME/programs/uncrustify/build/uncrustify -c $HOME/programs/neovim/contrib/uncrustify.cfg --replace --no-backup'
+alias un='uncrustify -c $HOME/programs/neovim/src/uncrustify.cfg --replace --no-backup'
+alias bun='$HOME/programs/uncrustify/build/uncrustify -c $HOME/programs/neovim/src/uncrustify.cfg --replace --no-backup'
 alias cla='clang-format -i *.{c,h} {api,eval,event,lua,msgpack_rpc,os,tui,viml}/**/*.{c,h}'
-alias una='uncrustify -c $HOME/programs/neovim/contrib/uncrustify.cfg --replace --no-backup *.{c,h} {api,eval,event,lua,msgpack_rpc,os,tui,viml}/**/*.{c,h}'
-alias buna='$HOME/programs/uncrustify/build/uncrustify -c $HOME/programs/neovim/contrib/uncrustify.cfg --replace --no-backup *.{c,h} {api,eval,event,lua,msgpack_rpc,os,tui,viml}/**/*.{c,h}'
+alias una='uncrustify -c $HOME/programs/neovim/src/uncrustify.cfg --replace --no-backup *.{c,h} {api,eval,event,lua,msgpack_rpc,os,tui,viml}/**/*.{c,h}'
+alias buna='$HOME/programs/uncrustify/build/uncrustify -c $HOME/programs/neovim/src/uncrustify.cfg --replace --no-backup *.{c,h} {api,eval,event,lua,msgpack_rpc,os,tui,viml}/**/*.{c,h}'
 alias clint='$HOME/programs/neovim/src/clint.py'
 alias clinta='$HOME/programs/neovim/src/clint.py *.{c,h} {api,eval,event,lua,msgpack_rpc,os,tui,viml}/**/*.{c,h}'
-alias unc-update='$HOME/programs/uncrustify/build/uncrustify -c $HOME/programs/neovim/contrib/uncrustify.cfg --update-config-with-doc -o $HOME/programs/neovim/contrib/uncrustify.cfg'
+alias unc-update='$HOME/programs/uncrustify/build/uncrustify -c $HOME/programs/neovim/src/uncrustify.cfg --update-config-with-doc -o $HOME/programs/neovim/src/uncrustify.cfg'
 
 format_files=(
   edit.c
