50efc01c628cdf2d2080a134fe29660dc26bc9ec
Dundar Göc <gocdundar@gmail.com>
2021-10-07 09:21:26 +0200

    change buildinstall to use release type

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -348,7 +348,7 @@ build() {
 buildinstall(){
 	local neovim_path="$HOME/programs/neovim"
 
-  build
+	make -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path" CMAKE_BUILD_TYPE=RelWithDebInfo
 	make -C "$neovim_path" install
 }
 
