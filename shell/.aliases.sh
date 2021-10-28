23c0cf40537adb283c06e3f286754adc262680c3
Dundar Göc <gocdundar@gmail.com>
2021-10-28 15:25:40 +0200

    asan: don't remove build when using build-asan

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -549,7 +549,6 @@ build-asan() (
 	neovim_path="$HOME/programs/neovim"
 	cd $neovim_path
 
-	rm -rf build
 	make CMAKE_INSTALL_PREFIX="$neovim_path" CMAKE_EXTRA_FLAGS="-DCMAKE_C_COMPILER=clang -DCLANG_ASAN_UBSAN=1" CMAKE_BUILD_TYPE=RelWithDebInfo
 	make install
 )
