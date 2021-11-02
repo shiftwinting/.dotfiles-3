a84dec39ca1555c8194e09673685afdeec3d4276
Dundar Göc <gocdundar@gmail.com>
2021-11-02 13:08:30 +0100

    fish: define necessary environment variables

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,4 +1,13 @@
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim 
 
+set -Ux XDG_CONFIG_HOME $HOME/.config
+set -Ux XDG_CACHE_HOME $HOME/.cache
+set -Ux XDG_DATA_HOME $HOME/.local/share
+set -Ux XDG_DATA_DIRS /usr/local/share:/usr/share
+set -Ux XDG_CONFIG_DIRS /etc/xdg
+
+# Change ls colors. Possible colors schemes: ayu, jellybeans, molokai, snazzy, solarized-dark, solarized-light.
+set -Ux LS_COLORS (vivid generate snazzy)
+
 setxkbmap -option "ctrl:nocaps"
